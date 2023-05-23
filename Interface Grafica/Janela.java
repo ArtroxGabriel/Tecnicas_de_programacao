848874bcf5b3b16bee7a40d02c037531f4e50860
Gabrigas <78170301+ArtroxGabriel@users.noreply.github.com>
2023-05-23 12:41:02 +0000

    Julia Calva

--- Interface Grafica/Janela.java
+++ Interface Grafica/Janela.java
@@ -13,11 +13,30 @@ class Janela extends Frame {
     Button b = new Button("OK");
     b.setForeground(Color.black);
     b.setBackground(Color.red);
-    b.setLocation(150, 150);
-    b.setSize(200, 100);
-    j.add(b, "south");
+    b.setLocation(100, 100);
+    b.setSize(50, 50);
+    j.add(b, "North");
 
-    j.setLayout(null);
+    Panel p = new Panel();
+    p.setBackground(Color.green);
+    j.add(p);
+
+    Label l = new Label("Suibanana");
+    l.setForeground(Color.white);
+    l.setBackground(Color.gray);
+    p.add(l);
+
+    TextField t = new TextField("Login: ",20);
+    t.setBackground(Color.pink);
+    p.add(t);
+
+    // j.setLayout(null);
     j.pack();
+  }
+
+  class TratadorJanela extends WindowListener {
+    public void windowClosing(WindowEvent e) {
+      System.exit(0);
+    }
   }
 }
