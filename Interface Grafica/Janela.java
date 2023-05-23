b29a1e053cb5b2f96009ddb64321e8aa5a3702a4
Gabrigas <78170301+ArtroxGabriel@users.noreply.github.com>
2023-05-23 12:20:12 +0000

    hello

---
+++ Interface Grafica/Janela.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+class Janela extends Frame {
+  Janela(String titulo) {
+    super(titulo);
+    setSize(500, 500);
+    setVisible(true);
+    this.setBackground(Color.white);
+  }
+
+  public static void main(String[] args) {
+    Janela j = new Janela("Minha Janela");
+    Button b = new Button("OK");
+    b.setForeground(Color.black);
+    b.setBackground(Color.red);
+    b.setLocation(150, 150);
+    b.setSize(200, 100);
+    j.add(b);
+
+    j.setLayout(null);
+    j.pack();
+  }
+}
