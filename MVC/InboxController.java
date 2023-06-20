7e47e2ccb7b51dae916c3a626f2deac8304c8835
Gabrigas <78170301+ArtroxGabriel@users.noreply.github.com>
2023-06-20 12:36:22 +0000

    preguica de dar git add

--- MVC/InboxController.java
+++ MVC/InboxController.java
@@ -1,6 +1,8 @@
 public class InboxController {
     InboxController(String message, String sender, String receiver) {
-
+        model.setSender(sender);
+        model.setReceiver(receiver);
+        model.setMessage(message);
     }
 
 }
